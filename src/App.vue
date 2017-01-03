eaf7824c9a4d1079355ff7115e8d73f915ae3cd5
pluto1114 <xiongmao1114@163.com>
2017-01-03 22:04:58 +0800

    改变背景色

--- src/App.vue
+++ src/App.vue
@@ -75,11 +75,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  background: 
-    linear-gradient(to bottom, #D5DEE7 0%, #E8EBF2 50%, #E2E7ED 100%),
-    linear-gradient(to bottom, rgba(0,0,0,0.02) 50%, rgba(255,255,255,0.02) 61%, rgba(0,0,0,0.02) 73%),
-    linear-gradient(33deg, rgba(255,255,255,0.20) 0%, rgba(0,0,0,0.20) 100%);
-  background-blend-mode: normal,color-burn;
+  background-color: #E1E6EC;
+  background-image: 
+      linear-gradient(-180deg, #E1E6EC 2%, #F0F4F7 96%),
+      radial-gradient(at 50% 100%, rgba(255,255,255,0.2) 0%, rgba(0,0,0,0.5) 200%);
+  background-blend-mode: normal, color-burn;
   
   min-height: 640px;
 }
